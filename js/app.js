200176d22a4d1f5289350f818ab184e183971dc2
Palak-SweetCoder <pspalak6@gmail.com>
2022-02-18 20:33:19 +0600

    all mistake resolved

--- js/app.js
+++ js/app.js
@@ -7,51 +7,56 @@ function userInput(inputId) {
 
 document.getElementById('calculate-button').addEventListener('click', function () {
     //all user input
-    const userInputValue = userInput('user-income');
+    const userIncome = userInput('user-income');
     const userFoodExpenseValue = userInput('food-expense');
     const userRentExpenseValue = userInput('rent-expense');
     const userClothExpenseValue = userInput('cloth-expense');
 
-    if (userInputValue > 0 && userFoodExpenseValue > 0 && userRentExpenseValue > 0 && userClothExpenseValue > 0) {
+    if (userIncome > 0 && userFoodExpenseValue > 0 && userRentExpenseValue > 0 && userClothExpenseValue > 0) {
         //determine total expense and amount balance
         const totalExpense = userFoodExpenseValue + userRentExpenseValue + userClothExpenseValue;
-        const newBalance = userInputValue - totalExpense;
-        //set total expense into the total expense field
-        const totalExpenses = document.getElementById('total-expenses');
-        totalExpenses.innerText = totalExpense;
-        //set amount balance into the amount balance field
-        const balance = document.getElementById('balance');
-        balance.innerText = newBalance;
-        const totalBalance = parseInt(newBalance);
 
-        //saving calculation
-        document.getElementById('save-button').addEventListener('click', function () {
-            //user input for saving
-            const saveInputValue = userInput('save-input');
+        if (totalExpense < userIncome) {
+            const newBalance = userIncome - totalExpense;
+            //set total expense into the total expense field
+            const totalExpenses = document.getElementById('total-expenses');
+            totalExpenses.innerText = totalExpense;
+            //set amount balance into the amount balance field
+            const balance = document.getElementById('balance');
+            balance.innerText = newBalance;
+            const totalBalance = parseInt(newBalance);
 
-            if (saveInputValue > 0) {
-                //calculate saving amount
-                const userSaving = (userInputValue * saveInputValue) / 100;
+            //saving calculation
+            document.getElementById('save-button').addEventListener('click', function () {
+                //user input for saving
+                const saveInputValue = userInput('save-input');
 
-                if (totalBalance > userSaving) {
-                    //set saving amount value into the saving amount field
-                    const savingAmountText = document.getElementById('saving-amount');
-                    savingAmountText.innerText = userSaving;
-                    //calculate remaining amount
-                    const amountBalance = totalBalance - userSaving;
-                    //set remaining amount into the Remaining Balance field
-                    const afterSavingAmountText = document.getElementById('after-saving-balance');
-                    afterSavingAmountText.innerText = amountBalance;
+                if (saveInputValue > 0) {
+                    //calculate saving amount
+                    const userSaving = (userIncome * saveInputValue) / 100;
+
+                    if (newBalance > userSaving) {
+                        //set saving amount value into the saving amount field
+                        const savingAmountText = document.getElementById('saving-amount');
+                        savingAmountText.innerText = userSaving;
+                        //calculate remaining amount
+                        const amountBalance = newBalance - userSaving;
+                        //set remaining amount into the Remaining Balance field
+                        const afterSavingAmountText = document.getElementById('after-saving-balance');
+                        afterSavingAmountText.innerText = amountBalance;
+                    }
+                    else {
+                        document.getElementById('validation-error3').style.display = 'block';
+                    }
                 }
                 else {
-                    document.getElementById('validation-error3').style.display = 'block';
+                    document.getElementById('validation-error2').style.display = 'block';
                 }
-
-            }
-            else {
-                document.getElementById('validation-error2').style.display = 'block';
-            }
-        })
+            })
+        }
+        else {
+            document.getElementById('validation-error4').style.display = 'block';
+        }
     }
     else {
         document.getElementById('validation-error').style.display = 'block';
